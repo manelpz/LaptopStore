ea640588225a8004d59e7ec0ad45bf19381862fd
Emmanuel López Guerrero <manelpz@icloud.com>
2020-10-11 22:33:23 -0500

    adding server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
+const http = require('http');
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
 
-console.log(__dirname);
-console.log(json);
+const server = http.createServer((req, res) => {
+    console.log('someone did access the server');
+});
+
+server.listen(1337, '127.0.0.1', ()=>{
+    console.log('Listening for request now');
+});
