e188c8c7ca48df0bdace061dd4a9b0f02144d429
Emmanuel López Guerrero <manelpz@icloud.com>
2020-10-14 19:54:33 -0500

    adding template function replace

--- index.js
+++ index.js
@@ -10,30 +10,26 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
    
+    //PRODUCT OVERVIEW
     if(pathName === '/products' || pathName === '/'){
         res.writeHead(200, {'Content-type': 'text/html'});
         res.end('This is the products');
     }
     
+    //LAPTOP DETAIL
     else if (pathName === '/laptop' && id < laptopData.length ){
         res.writeHead(200, {'Content-type': 'text/html'});
         
         fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf-8', (err, data)=>{
-            const laptop = laptopData[id];
-            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-             output = output.replace(/{%IMAGE%}/g, laptop.image);
-             output = output.replace(/{%PRICE%}/g, laptop.price);
-             output = output.replace(/{%SCREEN%}/g, laptop.screen);
-             output = output.replace(/{%CPU%}/g, laptop.cpu);
-             output = output.replace(/{%STORAGE%}/g, laptop.storage);
-             output = output.replace(/{%RAM%}/g, laptop.ram);
-             output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            const laptop = laptopData[id];  
+            const output = replaceTemplate(data, laptop);
 
             res.end(output);
         });
 
     }
     
+    //NOT FOUND
     else{
         res.writeHead(404, {'Content-type': 'text/html'});
         res.end('URL was no found');
@@ -46,3 +42,19 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', ()=>{
     console.log('Listening for request now');
 });
+
+
+function replaceTemplate(originalHtml, laptop){
+
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+};
