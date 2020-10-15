2a6a7d66fff07d996dfb06d63e6382afaffa0ff4
Emmanuel López Guerrero <manelpz@icloud.com>
2020-10-14 22:54:17 -0500

    addin routing images

--- index.js
+++ index.js
@@ -40,7 +40,15 @@ const server = http.createServer((req, res) => {
             res.end(output);
         });
     }
-    
+
+    //IMAGES
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)){
+        fs.readFile(`${__dirname}/data/img${pathName}`,(err, data)=>{
+            res.writeHead(200, {'Content-type': 'image/jpg'});
+            res.end(data);
+        });
+    }
+
     //NOT FOUND
     else{
         res.writeHead(404, {'Content-type': 'text/html'});
